dcf9e0eaeb4514c36a8959a2f85f937a9a547f82
talha naeem <talhanaeem.viralsquare@gmail.com>
2020-12-08 14:37:04 +0500

    first commit

--- spec/features/listing_players_spec.rb
+++ spec/features/listing_players_spec.rb
@@ -1,3 +1,16 @@
 require "rails_helper"
 
-RSpec.describe ""
+RSpec.describe "List Players" do
+  it "displays the first name and last name from the players saved" do
+    player1 = Player.create(first_name:"Eduardo", last_name:"Baik" )
+    player1 = Player.create(first_name:"Christopher", last_name:"Burns" )
+
+    visit "/"
+    expect(page).to have_text("Eduardo")
+    expect(page).to have_text("baik")
+    expect(page).to have_text("Christopher")
+    expect(page).to have_text("Burns")
+
+    expect(page).to have_link("New Player")
+  end
+end
